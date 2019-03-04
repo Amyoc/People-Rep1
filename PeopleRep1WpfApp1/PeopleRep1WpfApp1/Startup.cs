577338d4548d6a59bbf81813b459807d8d5dff0c
Amy OCallaghan <S00191048@mail.itsligo.ie>
2019-03-04 14:52:09 +0000

    Second update

--- PeopleRep1WpfApp1/PeopleRep1WpfApp1/Startup.cs
+++ PeopleRep1WpfApp1/PeopleRep1WpfApp1/Startup.cs
@@ -27,7 +27,7 @@ namespace PeopleRep1WpfApp1
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World TEST!");
             });
         }
     }
